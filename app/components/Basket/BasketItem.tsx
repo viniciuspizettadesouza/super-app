9104c7a7d1a1db4093aef4717476123bef2fc955
Vinicius Souza <viniciuspzt@gmail.com>
2023-09-07 16:54:18 +0100

    usestate for basketitem

--- app/components/Basket/BasketItem.tsx
+++ app/components/Basket/BasketItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { BasketItem, Product } from "@interfaces/currencies.interface";
 import { useBasketContext } from "@contexts/BasketContext";
@@ -10,18 +10,21 @@ interface BasketItemProps {
 }
 
 export default function BasketItem({ product }: BasketItemProps) {
+  const { id, name, image, price } = product;
   const { basket, setBasket } = useBasketContext();
-  const existingItem = basket.find((item: { id: string; }) => item.id === product.id);
-  const basketItem = existingItem?.quantity || 0;
+  const existingItem = basket.find((item: { id: string }) => item.id === id);
+  const [basketItem, setBasketItem] = useState(existingItem?.quantity || 0);
 
   const addToBasket = (id: string) => {
     if (existingItem) {
+      setBasketItem((prevQuantity: number) => prevQuantity + 1);
       setBasket((prevBasket: BasketItem[]) =>
         prevBasket.map((item) =>
           item.id === id ? { ...item, quantity: item.quantity + 1 } : item
         )
       );
     } else {
+      setBasketItem(1);
       setBasket((prevBasket: BasketItem[]) => [
         ...prevBasket,
         { ...product, quantity: 1 },
@@ -31,6 +34,7 @@ export default function BasketItem({ product }: BasketItemProps) {
 
   const decreaseFromBasket = (id: string) => {
     if (existingItem && existingItem.quantity > 1) {
+      setBasketItem((prevQuantity: number) => prevQuantity - 1);
       setBasket((prevBasket: BasketItem[]) =>
         prevBasket.map((item) =>
           item.id === id ? { ...item, quantity: item.quantity - 1 } : item
@@ -40,6 +44,7 @@ export default function BasketItem({ product }: BasketItemProps) {
   };
 
   const removeFromBasket = (id: string) => {
+    setBasketItem(0);
     setBasket((prevBasket: BasketItem[]) =>
       prevBasket.filter((item) => item.id !== id)
     );
@@ -49,8 +54,8 @@ export default function BasketItem({ product }: BasketItemProps) {
     <div className="flex justify-evenly my-2 mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
       <div className="w-32 h-32 relative">
         <Image
-          src={product.image}
-          alt={product.name}
+          src={image}
+          alt={name}
           fill
           className="rounded"
           sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 25vw"
@@ -59,13 +64,13 @@ export default function BasketItem({ product }: BasketItemProps) {
 
       <div className="ml-4 flex justify-between flex-col">
         <div className="mt-5 sm:mt-0">
-          <h2 className="text-lg font-bold text-gray-900">{product.name}</h2>
+          <h2 className="text-lg font-bold text-gray-900">{name}</h2>
         </div>
 
         <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
           <div className="flex items-center border-gray-100">
             <button
-              onClick={() => decreaseFromBasket(product.id)}
+              onClick={() => decreaseFromBasket(id)}
               className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
             >
               <FontAwesomeIcon
@@ -80,10 +85,11 @@ export default function BasketItem({ product }: BasketItemProps) {
               type="number"
               value={basketItem}
               min="0"
+              onChange={(e) => setBasketItem(e.target.value || 0)}
             />
 
             <button
-              onClick={() => addToBasket(product.id)}
+              onClick={() => addToBasket(id)}
               className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
             >
               <FontAwesomeIcon
@@ -95,10 +101,10 @@ export default function BasketItem({ product }: BasketItemProps) {
           </div>
 
           <div className="flex items-center space-x-4">
-            <p className="text-sm">${product.price.toFixed(2)}</p>
+            <p className="text-sm">${price.toFixed(2)}</p>
 
             <button
-              onClick={() => removeFromBasket(product.id)}
+              onClick={() => removeFromBasket(id)}
               className="cursor-pointer rounded py-1 px-3 duration-100 hover:bg-gray-100"
             >
               <FontAwesomeIcon
